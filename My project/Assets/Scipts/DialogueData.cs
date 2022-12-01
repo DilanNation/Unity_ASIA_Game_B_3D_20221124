1694b94b02117237211bc073745ebefd1ed33f51
JulianDilanda <113577991+DilanNation@users.noreply.github.com>
2022-12-01 16:04:44 +0800

    last update

---
+++ My project/Assets/Scipts/DialogueData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "KID/Dialogue Data", fileName = "New Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    [Header("對話者名稱")]
+    public string dialogueName;
+    [Header("對話者內容"), TextArea(2, 10)]
+    public string[] dialogueContent;
+}
